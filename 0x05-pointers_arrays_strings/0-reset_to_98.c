df3579cc674eab5cc5c455dcfbf31726fe4dd9a1
Somtochi <Sommychukwuma723@gmail.com>
2022-04-20 10:16:18 -0700

    fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 *reset_to_98 -updates a integer value to 98
 *@n: A pointer to an int that will be updated
 *
-*Return: void that means is correct 
+*Return: void that means is correct
 */
 void reset_to_98(int *n)
 {
