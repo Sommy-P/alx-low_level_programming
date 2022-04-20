0f3ee9632e8604958138822dadadd8ac0e1facf5
Somtochi <Sommychukwuma723@gmail.com>
2022-04-20 10:29:02 -0700

    fixed

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,11 +5,12 @@
 *@b: B pointer to an int that will be updated
 * Return: void that means our answer is correct
 */
-void swap_int(int *a, int *b)  
+void swap_int(int *a, int *b)
 {
 int f;
-f = *a;  
-*a = *b;  
-*b = f;  
+f = *a;
+*a = *b:
+*b = f;
 }
+
 
